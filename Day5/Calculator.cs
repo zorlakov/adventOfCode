9d3a77ea5886ad6bcc7f462177a8cf33d6413a1d
unknown <zoka_cs_pro@hotmail.com>
2021-05-22 00:38:41 +0200

    Day 5 solution added

---
+++ Day5/Calculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    class Calculator
+    {
+        public int Calculate(string[] seats)
+        {
+            int maxId = 0;
+
+            for (int i = 0; i < seats.Length; i++)
+            {
+
+                int maxRowValue = 127;
+                int minRowValue = 0;
+                int maxColumnValue = 7;
+                int minColumnValue = 0;
+                int currentId = 0;
+
+                foreach (var c in seats[i].ToCharArray())
+                {
+                    switch (c)
+                    {
+                        case 'F':
+                            maxRowValue -= ((maxRowValue - minRowValue) / 2) + 1;
+                            break;
+                        case 'L':
+                            maxColumnValue -= ((maxColumnValue - minColumnValue) / 2) + 1;
+                            break;
+                        case 'B':
+                            minRowValue += ((maxRowValue - minRowValue) / 2) + 1;
+                            break;
+                        case 'R':
+                            minColumnValue += ((maxColumnValue - minColumnValue) / 2) + 1;
+                            break;
+                        default:
+                            break;
+                    }
+                    currentId = minRowValue * 8 + minColumnValue;
+                    if (currentId > maxId) maxId = currentId;
+
+                }
+
+            }
+
+
+
+
+            return maxId;
+        }
+    }
+}
---
+++ Day5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] seats = System.IO.File.ReadAllLines("data.txt");
+            Calculator c = new Calculator();
+            int mySeat = c.Calculate(seats);
+            Console.WriteLine(mySeat);
+        }
+    }
+}
