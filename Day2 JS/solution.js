1a844238fa2f142e5221c6807647cb6e4cf227b4
unknown <zoka_cs_pro@hotmail.com>
2021-05-22 15:26:49 +0200

    Removed redundant comments

--- Day2 JS/solution.js
+++ Day2 JS/solution.js
@@ -1,25 +1,8 @@
 const fs = require('fs');
 
-const rows = fs.readFileSync('testData.txt').toString('utf-8').split('\n');
-var passwordsCount = 0;
-
-/* rows.forEach((row) => {
-  var dashCount = 0;
-  let minRepetitions = 0;
-  let maxRepetitions = 0;
-  let letter = 0;
-  for (let i = 0; i < row.length(); i++) {
-    if (row[i] == '-') dashCount++;
-    dashCount > 0
-      ? row[i] >= '0' && row[i] <= '9'
-        ? (minRepetitions += row[i])
-        : {}
-      : row[i] >= '0' && row[i] <= '9'
-      ? (maxRepetitions += row[i])
-      : {};
-  }
-});
- */
+const rows = fs.readFileSync('data.txt').toString('utf-8').split('\n');
+var passwordsCountFirst = 0;
+var passwordsCountSecond = 0;
 
 rows.forEach((row) => {
   // Replace all spaces and colons with a "-" character, then we split the string into four parts by using "-" as a parameter to the split function.
@@ -29,17 +12,21 @@ rows.forEach((row) => {
   var [minRepetitions, maxRepetitions, letter, password] = [
     parseInt(fields[0]),
     parseInt(fields[1]),
-    // fields[0],
-    //  fields[1],
     fields[2],
     fields[3],
   ];
 
-  // console.log(minRepetitions);
-
-  // We can use split() on the password string and send the letter that needs to be repeated N times as the argument to count how many times that letter repeats.
+  // We can use split() on the password string and send the letter that needs to be repeated N times as the argument to count how many times that letter repeats. (PART ONE SOLUTION)
   var count = password.split(letter).length - 1;
-  if (count >= minRepetitions && count <= maxRepetitions) passwordsCount++;
+  if (count >= minRepetitions && count <= maxRepetitions) passwordsCountFirst++;
+
+  // Part two solution:
+  if (
+    (password.charAt(minRepetitions) == letter) !==
+    (password.charAt(maxRepetitions) == letter)
+  )
+    passwordsCountSecond++;
 });
 
-console.log(passwordsCount);
+console.log(passwordsCountFirst + ' is the solution for part one');
+console.log(passwordsCountSecond + ' is the solution for part two');
