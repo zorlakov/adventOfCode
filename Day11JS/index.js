34bf71704190b3ad5dd3dbe5050e047528411ad6
unknown <zoka_cs_pro@hotmail.com>
2021-05-24 11:02:25 +0200

    removed old code from index.js in day11

--- Day11JS/index.js
+++ Day11JS/index.js
@@ -1,17 +1,11 @@
 // Navigate to "Day11JS folder" and run "node index" in terminal.
 // Import modules
-
-//const solution = require('./partOne');
-//const solution1 = require('./partTwo');
 const solution = require('./solution');
 
 const fs = require('fs');
 
 const seats = fs.readFileSync('data.txt').toString('utf-8').split('\n');
 
-// var first = solution.solveFirst(seats);
-
-// var second = solution1.solveSecond(seats);
 var first = solution.solve(seats, 4);
 var second = solution.solve(seats, 5);
 
