e7e59743578171ae793f6e2277ac85cdf1ccfeda
unknown <zoka_cs_pro@hotmail.com>
2021-05-24 10:47:17 +0200

    Combined the two day 11 solutions in 1 file, removed repeating code

--- Day11JS/index.js
+++ Day11JS/index.js
@@ -1,15 +1,19 @@
 // Navigate to "Day11JS folder" and run "node index" in terminal.
 // Import modules
-const solution = require('./partOne');
-const solution1 = require('./partTwo');
+
+//const solution = require('./partOne');
+//const solution1 = require('./partTwo');
+const solution = require('./solution');
 
 const fs = require('fs');
 
 const seats = fs.readFileSync('data.txt').toString('utf-8').split('\n');
 
-var first = solution.solveFirst(seats);
+// var first = solution.solveFirst(seats);
 
-var second = solution1.solveSecond(seats);
+// var second = solution1.solveSecond(seats);
+var first = solution.solve(seats, 4);
+var second = solution.solve(seats, 5);
 
 console.log(first);
 
--- Day11JS/solution.js
+++ Day11JS/solution.js
@@ -6,6 +6,28 @@ const countOccupiedSeats = (
   currentSeating
 ) => {
   let counter = 0;
+  // Check the 8 adjacent seats
+  for (let i = positionX - 1; i <= positionX + 1; i++) {
+    // If i or j go below 0 or above the number of rows/columns skip - out of bounds
+    if (i < 0 || i >= rows) continue;
+    for (let j = positionY - 1; j <= positionY + 1; j++) {
+      // Ignore current seat (when i and j are equal to position X and Y)
+      if (j < 0 || j >= columns - 1 || (i == positionX && j == positionY))
+        continue;
+      else if (currentSeating[i][j] == '#') counter++;
+    }
+  }
+  return counter;
+};
+
+const countOccupiedSeatsAll = (
+  positionX,
+  positionY,
+  rows,
+  columns,
+  currentSeating
+) => {
+  let counter = 0;
   let directions = [
     [-1, -1],
     [-1, 1],
@@ -49,12 +71,13 @@ const countOccupiedSeats = (
   return counter;
 };
 
-function solveSecond(seats) {
+function solve(seats, limit) {
   // 2D array equal to the input data
   var previousSeating = [[]];
   var rows = seats.length;
   var columns = seats[0].length;
   for (let i = 0; i < rows; i++) {
+    // NOTE: columns -1 instead of columns because puzzle data from adventofcode site copies with a " " at the end of all strings adding a new column.
     var line = new Array(columns - 1);
     for (let j = 0; j < columns - 1; j++) {
       line[j] = seats[i][j];
@@ -72,12 +95,17 @@ function solveSecond(seats) {
       }
       newSeating[i] = line;
     }
+
     for (let i = 0; i < rows; i++) {
       for (let j = 0; j < columns - 1; j++) {
         if (previousSeating[i][j] == '.') continue;
+        let counter;
+        if (limit == 4)
+          counter = countOccupiedSeats(i, j, rows, columns, previousSeating);
+        else
+          counter = countOccupiedSeatsAll(i, j, rows, columns, previousSeating);
 
-        let counter = countOccupiedSeats(i, j, rows, columns, previousSeating);
-        if (previousSeating[i][j] == '#' && counter >= 5)
+        if (previousSeating[i][j] == '#' && counter >= limit)
           newSeating[i][j] = 'L';
         if (previousSeating[i][j] == 'L' && counter == 0)
           newSeating[i][j] = '#';
@@ -107,4 +135,4 @@ function solveSecond(seats) {
   }
 }
 
-module.exports = { solveSecond };
+module.exports = { solve };
